cb6888431cb9dbbccb287e9bd7a23eaefbf79ef9
philippeduhamel11 <philippeduhamel11@gmail.com>
2024-06-25 15:50:49 +0200

    Fusion craft inventaire

--- WelcomeToBury_unity/Assets/Scripts/System Scripts/Crafting/_Sc_craftSlot.cs
+++ WelcomeToBury_unity/Assets/Scripts/System Scripts/Crafting/_Sc_craftSlot.cs
@@ -6,4 +6,21 @@ public class _Sc_craftSlot : MonoBehaviour
 {
     public _So_item _item = null;
     public int index = 0;
+
+    _Sc_tooltipTrigger _sc_tolltipTrigger = null;
+
+    private void Awake()
+    {
+        _sc_tolltipTrigger = transform.GetChild(0).GetComponent<_Sc_tooltipTrigger>();
+    }
+    private void Start()
+    {
+        if(_sc_tolltipTrigger != null)
+        {
+            if(_item != null)
+            {
+                _sc_tolltipTrigger.header = _item.itemName.ToString();
+            }
+        }
+    }
 }
